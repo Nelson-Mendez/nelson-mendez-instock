f3a04b9f3d5c05c419658ded60e8efd5d1d4b431
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-02 23:01:13 -0500

    added status and error conditional statements and msg to check for malformed or missing data in fields

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -2,18 +2,13 @@ const express = require("express");
 const router = express.Router();
 const locations = require("../data/locations.json");
 
-// create an endpoint that will create a new warehouse
-// if the data is malformed or if a field is missing, return a 400 status, rejecting the POST request
-// return new list of warehouses back to the front-end on success
-
-
 router.post("/", (req, res) => {
   const data = req.body;
   const name = data.name;
   const id = data.id;
   const street = data.street;
   const location = data.location;
-  const contact = data.contact;
+  const contactName = data.contactName;
   const position = data.position;
   const phone = data.phone;
   const email = data.email;
@@ -27,7 +22,7 @@ router.post("/", (req, res) => {
 
   const contact =
   {
-    contact: contact,
+    contact: contactName,
     position: position,
     phone: phone,
     email: email
@@ -42,6 +37,26 @@ router.post("/", (req, res) => {
     inventoryCategories: categories
   }
 
+  Object.values(uploadWarehouseObj).forEach(value => {
+    if (!value) {
+      return res.status(400).send("rejected: malformed data or data missing from a field")
+    }
+  })
+
+  Object.values(uploadWarehouseObj.address).forEach(value => {
+    if (!value) {
+      return res.status(400).send("rejected: malformed data or data missing from a field")
+    }
+  })
+
+  Object.values(uploadWarehouseObj.contact).forEach(value => {
+    if (!value) {
+      return res.status(400).send("rejected: malformed data or data missing from a field")
+    }
+  })
+
   locations.unshift(uploadWarehouseObj);
   res.send(locations);
 })
+
+module.exports = router;
