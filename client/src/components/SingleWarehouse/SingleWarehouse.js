03f3f69e71eff2500d2d2e05d06ae0ac39bd17ac
Nelson Mendez <nelmen2509@gmail.com>
2020-03-04 15:40:05 -0500

    Fixed pathing/routing, implemented product list component in the warehouse details view.

--- client/src/components/SingleWarehouse/SingleWarehouse.js
+++ client/src/components/SingleWarehouse/SingleWarehouse.js
@@ -6,33 +6,40 @@ import ProductList from '../../components/ProductList/ProductList.js';
 class SingleWarehouse extends React.Component {
     state = {
         warehouseData: [],
+        loadedData: false
     }
 
     componentDidMount () {
-        axios
-        .get('http://localhost:8080/warehouses/:id')
+        axios  
+        .get(`http://localhost:8080/warehouses/${this.props.match.params.id}`)
         .then(response => {
             console.log(response.data);
             this.setState ({
                 warehouseData: response.data,
+                loadedData: true
             })
         })
     }
+
     render () {
-        return (
-            <>
-            <h1>{this.state.warehouseData.name}</h1>
-            <h4>Address</h4>
-                <p>{this.state.warehouseData.address.street}</p>
-                <p>{this.state.warehouseData.address.location}</p>
-            <h4>Contact</h4>
-                <p>{this.state.warehouseData.contact.name}</p>
-                <p>{this.state.warehouseData.contact.position}</p>
-                <p>{this.state.warehouseData.contact.phone}</p>
-                <p>{this.state.warehouseData.contact.email}</p>
-            <ProductList content={this.state.warehouseData.inventory} />
-            </>
-        )
+
+        if (this.state.loadedData) {
+            return (
+                <>
+                    <h1>{this.state.warehouseData.name}</h1>
+                    <h4>Address</h4>
+                        <p>{this.state.warehouseData.address.street}</p>
+                        <p>{this.state.warehouseData.address.location}</p>
+                    <h4>Contact</h4>
+                        <p>{this.state.warehouseData.contact.name}</p>
+                        <p>{this.state.warehouseData.contact.position}</p>
+                        <p>{this.state.warehouseData.contact.phone}</p>
+                        <p>{this.state.warehouseData.contact.email}</p>
+                    <ProductList content={this.state.warehouseData.inventory} />
+                </>
+            )
+        }
+        else {return "loading"}
     }
 };
 
