e2fec5be7b93c567cbef34f2a3ad2297711f0cfb
Nelson Mendez <31571728+Nelson-Mendez@users.noreply.github.com>
2020-03-03 22:53:02 -0500

    Added Inventory Page
    Page is a class component which renders a list of inventory items, with each item given its details through props. Axios.get request to the api receives the full inventory list successfully.

---
+++ client/src/components/ModalButton/ModalButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './modalbutton.scss'
+
+
+export default function ModalButton () {
+    return(
+        <button className="addButton" />
+    )
+}
+
+
---
+++ client/src/pages/AllInventory/AllInventory.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import SingleProduct from '../../components/SingleProduct/SingleProduct';
+import ModalButton from '../../components/ModalButton/ModalButton';
+
+
+export default class AllInventory extends React.Component {
+
+    state = {
+        modalClicked: false,
+        inventoryList: [],
+        loadedInventory: false
+    }
+
+    componentDidMount () {
+        axios
+        .get('http://localhost:8080/inventory')
+        .then(response => {
+            this.setState ({
+                inventoryList: response.data,
+                loadedInventory: true
+            })
+        })
+    }
+
+    render () {
+
+        if (this.state.loadedInventory) {
+            return (
+                <main className="inventory">
+
+                    <h1 className="inventory__title">Inventory</h1>
+
+                    <SearchBar />
+
+                    <TableHeader />
+    
+                    {this.state.inventoryList.forEach(item => {
+                        return (
+                            <Link key={item.id} to={`/${item.id}`}>
+                                <SingleProduct content={item} />
+                            </Link>
+                        )
+                    })}
+
+                    <ModalButton />
+                    
+                </main>
+            )
+        }
+    }
+}
+
+
+
+// The user must be able to see a list of all inventory on the inventory tab. Each item has a name, description,
+//  last ordered, location, quantity and status information. Each item also has a 3-dot ellipsis kebab menu  on the far 
+//  right. Clicking an item in this view must take the user to the inventory details view.
+
+// Create the UI and functionality for displaying the list of inventory items.
+
+// This component will need state.
+
+// Be sure to create a single item component that displays one row in this list.
+
+// This component will also be used to display the inventory for a single warehouse. Your fetch to the back-end in this 
+// component will differ depending on if a warehouse ID is provided or not provided. Be sure to collaborate with the person
+//  who has the ticket “_Front-End: Warehouse Inventory List_“.
+
+// Note: If the inventory endpoint on the back-end is not complete, use fake data until it is ready to be hooked up.
