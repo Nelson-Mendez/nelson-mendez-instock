6ad0066947054d6a9726642645ae083463eca714
Ruina Zhang <rzhang46@uwo.ca>
2020-03-04 09:51:40 -0500

    created react router, installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav/Nav'; 
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import AllWarehouses from './pages/AllWarehouses/AllWarehouses'; 
+import AllInventory from './pages/AllInventory/AllInventory'; 
+import OneWarehouse from './pages/OneWarehouse/OneWarehouse';
+import OneProduct from './pages/OneProduct/OneProduct';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Redirect exact from='/' to='/warehouses' />
+          <Route path='/' component={AllWarehouses}/>
+          <Route path='/inventory' component={AllInventory} />
+          <Route path='/warehouses/:id' component={OneWarehouse} />
+          <Route path='/inventory/:id' component={OneProduct} /> 
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
