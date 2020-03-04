8d4d0924c6c2731607a961f7815c01047ad432bc
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-04 13:01:09 -0500

    removed cors header and changed to use in server.js, fixed conflicts and tested browser render for all warehouses

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/Nav/Nav'; 
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
-import AllWarehouses from './pages/AllWarehouses/AllWarehouses'; 
-import AllInventory from './pages/AllInventory/AllInventory'; 
+import Nav from './components/Nav/Nav';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AllWarehouses from './pages/AllWarehouses/AllWarehouses';
+import AllInventory from './pages/AllInventory/AllInventory';
 import OneWarehouse from './pages/OneWarehouse/OneWarehouse';
 import OneProduct from './pages/OneProduct/OneProduct';
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <BrowserRouter>
         <Nav />
         <Switch>
           <Route path='/warehouses/:id' component={OneWarehouse} />
-          <Route path='/inventory/:id' component={OneProduct} /> 
+          <Route path='/inventory/:id' component={OneProduct} />
           <Route path='/inventory' component={AllInventory} />
-          <Route exact path='/' component={AllWarehouses}/>
+          <Route path='/warehouses' component={AllWarehouses} />
+          <Route exact path='/' component={AllWarehouses} />
           <Redirect exact from='/' to='/warehouses' />
         </Switch>
       </BrowserRouter>
