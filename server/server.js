3142341b687874374edfacbd12324c910de03537
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-04 13:13:58 -0500

    removed cors header and used app use for cors

--- server/server.js
+++ server/server.js
@@ -7,11 +7,8 @@ const inventoryRouter = require('./routes/inventoryRoutes');
 
 
 // CORS middleware
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors());
 
 
 // Express.json middleware
