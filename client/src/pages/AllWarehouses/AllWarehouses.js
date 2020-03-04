7b819d1e7bfb944105defc66a5c1c0ef08075690
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-03 19:43:05 -0500

    added map function to generate warehouse list from each object

--- client/src/pages/AllWarehouses/AllWarehouses.js
+++ client/src/pages/AllWarehouses/AllWarehouses.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./allwarehouses.scss";
+import OneWarehouse from "../OneWarehouse/OneWarehouse";
 
 const URL = "http://localhost:8080/"
 
@@ -16,12 +17,44 @@ class AllWarehouses extends React.Component {
   getWarehouseList = () => {
     axios
       .get(`${URL}warehouses`)
-      .then
+      .then(res => {
+        this.setState({
+          warehouseList: res.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getWarehouseList();
   }
 
   render() {
     return (
       <>
+        <div className="warehouses__list">
+          <div className="warehouses__title-wrap">
+            <h1 className="warehouses__title">Locations</h1>
+            <p>SEARCH BAR SHOULD GO HERE</p>
+          </div>
+          {this.state.warehouseList.map(obj => {
+            return (
+              <div className="warehouses__item" key={obj.id}>
+                <Link to={`/warehouses/${obj.id}`}>
+                  <OneWarehouse
+                    id={obj.id}
+                    name={obj.name}
+                    address={obj.address}
+                    contact={obj.contact}
+                    categories={obj.inventoryCategories}
+                  />
+                </Link>
+              </div>
+            )
+          })}
+        </div>
       </>
     )
   }
