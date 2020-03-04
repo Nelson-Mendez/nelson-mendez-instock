ba67ec57e5ef9086d9840ef28f5b4be9b210c678
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-04 15:11:06 -0500

    completed mobile styles for AllWarehouses

--- client/src/pages/OneWarehouse/OneWarehouse.js
+++ client/src/pages/OneWarehouse/OneWarehouse.js
@@ -7,19 +7,21 @@ const OneWarehouse = ({ id, name, address, contact, categories }) => {
   return (
     <>
       <div className="warehouseItem__block warehouseItem__block--first">
-        <h2 className="warehouseItem__heading">{name}</h2>
-        <p className="warehouseItem__info">{address.street}, {address.location}</p>
-        <img className="warehouseItem__image" src={arrowright} />
+        <div className="warehouseItem__textBox">
+          <h2 className="warehouseItem__heading">{name}</h2>
+          <p className="warehouseItem__info">{address.street}, {address.location}</p>
+        </div>
+        <img className="warehouseItem__icon" src={arrowright} />
       </div>
       <div className="warehouseItem__block">
         <p className="warehouseItem__info">{contact.name}</p>
         <p className="warehouseItem__info warehouseItem__info--italic">{contact.position}</p>
       </div>
       <div className="warehouseItem__block">
-        <p className="warehouseItem__info">{contact.phone}</p>
+        <p className="warehouseItem__info">+1 {contact.phone}</p>
         <p className="warehouseItem__info">{contact.email}</p>
       </div>
-      <div className="warehouseItem__block">
+      <div className="warehouseItem__block warehouseItem__block--last">
         <p className="warehouseItem__info">{categories}</p>
       </div>
     </>
