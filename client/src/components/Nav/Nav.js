54573201198ffbf88b8abe13444874ceefde3799
Laith Harb <harblaith77@gmail.com>
2020-03-03 20:29:50 -0500

    Change a tags to Links

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import './nav.scss';
-import logo from '../../assets/Logo/Logo-instock.svg'
+import logo from '../../assets/Logo/Logo-instock.svg';
+import {Link} from 'react-router-dom'
+
 
 export default function Nav() {
     return (
         <nav class="Header">
             <ul class="Header__nav-list">
                 <li class="Header__nav-list-item Header__nav-list-item--logo">
-                    <a href="" class="Header__nav-link Header__nav-link--logo">
+                    <Link to="/" class="Header__nav-link Header__nav-link--logo">
                         <img src={logo} alt="" className="Header__logo"/>
-                    </a>
+                    </Link>
                 </li>
                 <div class="Header__links-container">
                     <li class="Header__nav-list-item Header__nav-list--links">
-                        <a href="./index.html" class="Header__nav-link Header__nav-link--active">
+                        <Link to="/inventory" class="Header__nav-link Header__nav-link--active">
                             <span> Inventory</span>
-                        </a>
+                        </Link>
                     </li>
                     <li class="Header__nav-list-item Header__nav-list--links">
-                        <a href="./shows.html" class="Header__nav-link Header__nav-link--location">
+                        <Link to="/locations" class="Header__nav-link Header__nav-link--location">
                             Locations
-                        </a>
+                        </Link>
                     </li>
                 </div>
             </ul>
