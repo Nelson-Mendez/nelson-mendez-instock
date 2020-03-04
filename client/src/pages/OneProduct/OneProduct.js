0d45d6010583c11b34f4b2c010a02ac9a762f2bf
Laith Harb <harblaith77@gmail.com>
2020-03-04 16:07:17 -0500

    Styled all pages on all breakpoints

--- client/src/pages/OneProduct/OneProduct.js
+++ client/src/pages/OneProduct/OneProduct.js
@@ -4,6 +4,7 @@ import './oneproduct.scss';
 import backButton from '../../assets/Icons/SVG/Icon-back-arrow.svg';
 import {Link} from 'react-router-dom';
 
+
 class OneProduct extends Component {
 
     constructor(props){
@@ -68,68 +69,76 @@ class OneProduct extends Component {
                             </button>
                         )}
                    </div>
+                    
+                    <div className="OneProduct__all-sections-container">
+                        <div className="OneProduct__section-container OneProduct__section-container--description">
+                            <h3 className="OneProduct__title">
+                                Item Description
+                            </h3>
+                            <p className="OneProduct__info">
+                                {this.state.itemData.description}
+                            </p>
+                        </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                           Item Description
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.description}
-                       </p>
-                   </div>
+                        <div className="OneProduct__information-container">
+                            <div className="OneProduct__sections-container">
+                                <div className="OneProduct__section-container">
+                                    <h3 className="OneProduct__title">
+                                            ORDERED BY
+                                    </h3>
+                                    <p className="OneProduct__info">
+                                            {this.state.warehouseData.contact && this.state.warehouseData.contact.name}
+                                    </p>
+                                </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                            ORDERED BY
-                       </h3>
-                       <p className="OneProduct__info">
-                            {this.state.warehouseData.contact && this.state.warehouseData.contact.name}
-                       </p>
-                   </div>
+                                <div className="OneProduct__section-container">
+                                    <h3 className="OneProduct__title">
+                                        Reference Number
+                                    </h3>
+                                    <p className="OneProduct__info">
+                                        {this.state.itemData.id}
+                                    </p>
+                                </div>
+                            </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                           Reference Number
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.id}
-                       </p>
-                   </div>
+                            <div className="OneProduct__sections-container">
+                                <div className="OneProduct__section-container">
+                                    <h3 className="OneProduct__title">
+                                        Last Ordered
+                                    </h3>
+                                    <p className="OneProduct__info">
+                                        {this.state.itemData.lastOrdered}
+                                    </p>
+                                </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                           Last Ordered
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.lastOrdered}
-                       </p>
-                   </div>
+                                <div className="OneProduct__section-container">
+                                    <h3 className="OneProduct__title">
+                                        Location
+                                    </h3>
+                                    <p className="OneProduct__info">
+                                        {this.state.itemData.city}
+                                    </p>
+                                </div>
+                            </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                           Location
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.city}
-                       </p>
-                   </div>
+                            <div className="OneProduct__section-container">
+                                <h3 className="OneProduct__title">
+                                        QUANTITY
+                                </h3>
+                                <p className="OneProduct__info">
+                                    {this.state.itemData.quantity}
+                                </p>
+                            </div>
 
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                            QUANTITY
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.quantity}
-                       </p>
-                   </div>
-
-                   <div className="OneProduct__section-container">
-                       <h3 className="OneProduct__title">
-                           Categories
-                       </h3>
-                       <p className="OneProduct__info">
-                           {this.state.itemData.categories}
-                       </p>
+                            <div className="OneProduct__section-container OneProduct__section-container--category">
+                                <h3 className="OneProduct__title">
+                                    Categories
+                                </h3>
+                                <p className="OneProduct__info">
+                                    {this.state.itemData.categories}
+                                </p>
+                            </div>
+                        </div>
                    </div>
 
                    <button className="OneProduct__edit-btn">
