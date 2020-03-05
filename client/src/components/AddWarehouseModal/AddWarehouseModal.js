4d0cc57b7f846b006444776e31bfc504ac56d9c9
Tiffany Chan <tiffanyvchan@gmail.com>
2020-03-05 00:31:34 -0500

    added form elements

--- client/src/components/AddWarehouseModal/AddWarehouseModal.js
+++ client/src/components/AddWarehouseModal/AddWarehouseModal.js
@@ -20,25 +20,60 @@ const AddWarehouseModal = ({ isOpen, closeModal }) => {
       {isOpen &&
         <Modal isOpen={isOpen}>
           <h2 className="modal-form__heading">Add New</h2>
-          <form>
-            <div className="modal-form__data">
-              <label className="modal-form__label">WAREHOUSE</label>
-              <input className="modal-form__name" name="name" placeholder="Name" required />
-              <input className="modal-form__name" name="name" placeholder="Id" required />
+          <form className="modal-form">
+            <div className="modal-form__row">
+              <div className="modal-form__field">
+                <label className="modal-form__label">WAREHOUSE</label>
+                <input className="modal-form__name" name="name" placeholder="Name" required />
+              </div>
+              <div className="modal-form__field">
+                <input className="modal-form__name" name="name" placeholder="Id" required />
+              </div>
             </div>
-            <div className="modal-form__data">
-              <label className="modal-form__label">ADDRESS</label>
-              <input className="modal-form__name" name="name" placeholder="Name" required />
-              <label className="modal-form__label">LOCATION</label>
-              <input className="modal-form__name" name="name" placeholder="Name" required />
+            <div className="modal-form__row">
+              <div className="modal-form__field">
+                <label className="modal-form__label">ADDRESS</label>
+                <input className="modal-form__name" name="name" placeholder="Address" required />
+              </div>
+              <div className="modal-form__field">
+                <label className="modal-form__label">LOCATION</label>
+                <input className="modal-form__name" name="name" placeholder="Location" required />
+              </div>
             </div>
-            {/* <textarea
-                name="comment"
-                type="text"
-                className="form__input"
-                placeholder="That was easily the most spectacular BMX moment ever."
-                required
-              /> */}
+            <div className="modal-form__row">
+              <div className="modal-form__field">
+                <label className="modal-form__label">CONTACT NAME</label>
+                <input className="modal-form__name" name="name" placeholder="Enter Name" required />
+              </div>
+              <div className="modal-form__field">
+                <label className="modal-form__label">POSITION</label>
+                <input className="modal-form__name" name="name" placeholder="Enter Position" required />
+              </div>
+            </div>
+            <div className="modal-form__row">
+              <div className="modal-form__field">
+                <label className="modal-form__label">PHONE NUMBER</label>
+                <input className="modal-form__name" name="name" placeholder="(000) - 000 - 0000" required />
+              </div>
+              <div className="modal-form__field">
+                <label className="modal-form__label">EMAIL</label>
+                <input className="modal-form__name" name="name" placeholder="email@instock.com" required />
+              </div>
+            </div>
+            <div className="modal-form__row">
+              <div className="modal-form__field">
+                <label className="modal-form__label">CATEGORIES</label>
+                <textarea
+                  name="categories"
+                  type="text"
+                  className="form__input"
+                  placeholder="Use commas to separate each category"
+                  required
+                />
+              </div>
+            </div>
+
+
           </form>
           <button onClick={closeModal}>Cancel</button>
         </Modal>
